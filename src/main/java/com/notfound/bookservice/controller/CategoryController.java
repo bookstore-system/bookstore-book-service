83d1737e9e04b980efe0666e4aa8c308c697148f
TruongIKPK <phitruong20192020@gmail.com>
2026-05-15 01:55:11 +0700

    feat: add create category endpoint and new CreateAuthorRequest DTO

--- src/main/java/com/notfound/bookservice/controller/CategoryController.java
+++ src/main/java/com/notfound/bookservice/controller/CategoryController.java
@@ -1,11 +1,15 @@
 package com.notfound.bookservice.controller;
 
+import com.notfound.bookservice.model.dto.request.CreateCategoryRequest;
 import com.notfound.bookservice.model.dto.response.ApiResponse;
 import com.notfound.bookservice.model.dto.response.CategoryResponse;
 import com.notfound.bookservice.service.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +24,11 @@ public class CategoryController {
     @GetMapping
     public ResponseEntity<ApiResponse<List<CategoryResponse>>> getCategories() {
         return ResponseEntity.ok(ApiResponse.success(bookService.getCategories()));
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<CategoryResponse>> createCategory(
+            @Valid @RequestBody CreateCategoryRequest request) {
+        return ResponseEntity.ok(ApiResponse.success(bookService.createCategory(request)));
     }
 }
